3b28f8b4c2f08b0c4c33a1be8755b1a2387716ea
Dinesh <dineshlegha23@gmail.com>
2024-08-30 17:59:40 +0530

    added delete song button

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { assets } from "../assets/assets";
 
-const Song = ({ name, album, image, duration }) => {
+const Song = ({ _id: id, name, desc, duration, image, deleteSong }) => {
   return (
     <div className="grid grid-cols-[1fr_3fr_2fr_1fr_1fr] items-center pl-2 border py-2">
       <img
@@ -10,9 +10,11 @@ const Song = ({ name, album, image, duration }) => {
         alt="song image"
       />
       <p>{name}</p>
-      <p className="text-zinc-500">{album}</p>
-      <p className="text-zinc-500 pl-2">{duration}</p>
-      <p className="pl-4">X</p>
+      <p className="text-zinc-500">{desc}</p>
+      <p className="text-zinc-5000 pl-2">{duration}</p>
+      <p className="pl-4 cursor-pointer" onClick={() => deleteSong(id)}>
+        X
+      </p>
     </div>
   );
 };
