0d2dec20d43716d11ee44339f936d7244acf4b91
Dinesh <dineshlegha23@gmail.com>
2024-08-30 16:39:48 +0530

    added list song

--- src/pages/ListSong.jsx
+++ src/pages/ListSong.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { assets } from "../assets/assets";
+import axios from "axios";
+import Song from "../components/Song";
 
 const ListSong = () => {
-  return <div>ListSong</div>;
+  const [songs, setSongs] = useState();
+
+  const fetchSongs = async () => {
+    const response = await axios("http://localhost:5000/api/v1/songs");
+    setSongs(response.data.data);
+  };
+
+  useEffect(() => {
+    fetchSongs();
+  }, []);
+
+  return (
+    <section>
+      <p className="my-6">All Songs List</p>
+      <div className="border-2">
+        <div className="grid grid-cols-[1fr_3fr_2fr_1fr_1fr] bg-gray-300 py-2 pl-2">
+          <p>Image</p>
+          <p>Name</p>
+          <p>Album</p>
+          <p>Duration</p>
+          <p>Action</p>
+        </div>
+        <div>
+          {songs &&
+            songs.map((song) => {
+              return <Song {...song} key={song._id} />;
+            })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ListSong;
